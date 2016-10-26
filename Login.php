d364f9d139da5d56dd6aaa89bb17742caf6eeb19
JhorelSula <Jhorel-Sula@uiowa.edu>
2016-10-26 12:16:59 -0500

    Updated Login
    This login has a picture and checks for completed fields. Not connected to db and or rest of site

--- Login.php
+++ Login.php
@@ -1,7 +1,46 @@
 <html>
+
+
+
+
+
+
+<?php
+//
+// Code to handle input from form 
+//
+if(isset($_POST['submit'])){
+	// only run if form submitted
+	
+	$Email = $_POST['Email'];
+	$Password = $_POST['Password'];
+
+	if(!$Director){
+		punt('Please enter Email.');
+	}
+	if(!$MovieTitle){
+		punt('Please enter Password.');
+	}
+
+
+	
+	$insert = 'INSERT INTO Movies(Director,MovieTitle,Year,url)VALUES ("'. $Director .'","'. $MovieTitle .'" ,"'. $Year .'" , "'. $url .'");';
+	//connect to datebase
+	$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+	
+	// run insert statement
+	$result = queryDB($insert, $db);
+	
+	//echo("Successfully entered")
+	
+}
+
+?>
+
+
 <form action="action_page.php">
   <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    <img src="Earth.jpg" alt="Avatar" class="avatar">
   </div>
 
   <div class="container">
@@ -12,12 +51,10 @@
     <input type="password" placeholder="Enter Password" name="psw" required>
 
     <button type="submit">Login</button>
-    <input type="checkbox" checked="checked"> Remember me
   </div>
 
   <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
+
   </div>
 </form>
 </html>
